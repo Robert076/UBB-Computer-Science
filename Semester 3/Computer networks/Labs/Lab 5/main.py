4dbe24caa2545053055d5d4b8eab5cda05e7d517
Robert076 <robertberes06@gmail.com>
2024-11-04 15:55:54 +0200

    done lab

--- Semester 3/Computer networks/Labs/Lab 5/main.py
+++ Semester 3/Computer networks/Labs/Lab 5/main.py
@@ -2,59 +2,72 @@ import socket
 import time
 import sys
 import threading
+import re
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-broadcast_address = "255.255.255.255"
+broadcastAddress = "255.255.255.255"
 port = 7777
 s.bind(('0.0.0.0', port))
-date_query_string = b"DATEQUERY\10"
-time_query_string = b"TIMEQUERY\10"
+dateQueryString = b"DATEQUERY\10"
+timeQueryString = b"TIMEQUERY\10"
+peersList = dict() 
+malformed = []
 
 def sendDateQuery():
-    global s, port, date_query_string, broadcast_address
+    global s, port, dateQueryString, broadcastAddress
     while True:
-        b = s.sendto(date_query_string, (broadcast_address, port))
-        if b != len(date_query_string):
+        b = s.sendto(dateQueryString, (broadcastAddress, port))
+        if b != len(dateQueryString):
             print("failed to send date query")
         time.sleep(3)
 
 def sendTimeQuery():
-    global s, port, time_query_string, broadcast_address
+    global s, port, timeQueryString, broadcastAddress
     while True:
-        b = s.sendto(time_query_string, (broadcast_address, port))
-        if b != len(time_query_string):
+        b = s.sendto(timeQueryString, (broadcastAddress, port))
+        if b != len(timeQueryString):
             print("failed to send time query")
         time.sleep(10)
 
 def responseQuery():
-    global s, port, time_query_string, date_query_string
+    global s, port, timeQueryString, dateQueryString, peersList
 
     while True:
         msg, addr = s.recvfrom(1024)
         print("Message received: " + msg.decode())
-
-        if msg == time_query_string:
+        if msg == timeQueryString:
             my_time = time.strftime("TIME %H:%M:%S")
             print(f"Responding with {my_time} to ip: {addr[0]} port {addr[1]}")
             my_time = my_time.encode()
             b = s.sendto(my_time, addr)
             if b != len(my_time):
                 print("failed to send response")
-        elif msg == date_query_string:
+        elif msg == dateQueryString:
             my_date = time.strftime("DATE %d:%m:%Y")
             print(f"Responding with {my_date} to ip: {addr[0]} port {addr[1]}")
             my_date = my_date.encode()
             b = s.sendto(my_date, addr)
             if b != len(my_date):
                 print("failed to send response")
+        else:
+            if regexMatch(msg = msg, pattern = "TIME [0-9]{2}:[0-9]{2}:[0-9]{2}") is None:
+                malformed.append(msg)
+            # inca unu asa pt data
+            # si dupa inca un thread pt afisare
+                
+                
+        
+def regexMatch(msg, pattern):
+    pattern = re.compile(pattern)
+    return pattern.fullmatch(msg)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Specify the proadcast address")
+        print("Specify the broadcast address")
         exit(1)
-    broadcast_address = sys.argv[1]
+    broadcastAddress = sys.argv[1]
     sndTimeThrd = threading.Thread(target=sendTimeQuery)
     sndDateThrd = threading.Thread(target=sendDateQuery)
 
@@ -65,4 +78,3 @@ if __name__ == "__main__":
 
     sndTimeThrd.join()
     sndDateThrd.join()
-
