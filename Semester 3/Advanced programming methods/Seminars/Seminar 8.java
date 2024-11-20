499ff9198d3c729a15e6b2d1efd85ae8ba706c31
Robert076 <robertberes06@gmail.com>
2024-11-20 12:02:22 +0200

    added seminar 8

--- Semester 3/Advanced programming methods/Seminars/Seminar 8.java
+++ Semester 3/Advanced programming methods/Seminars/Seminar 8.java
@@ -1,33 +1,7 @@
+List<Integer> numbers = Array.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
 // P1)
+numbers.stream().filter(num -> num % 2 == 0 || num % 3 == 0).map(n -> "A" + (n + 1).toString() + "B").reduce(" ", (acc, current) -> acc + current);
 
-Stringstream s;
-words.stream().forEach(word -> System.out.println("  " + word));
-
-// f3(f2(f1(x))) -> functional way
-// for (word in words) ... imperative way
-
-
-// P3)
-
-excitingWords = words.stream().map(s->s + "!").collect(collectors.toList())
-
-// P5)
-
-newWords = words.stream().map(String::toUpperCase).filter(s->s.length() < 4).filter(s->s.contains("E")).findFirst().orElse("None");
-
-// P6)
-words.stream().reduce(" ", (partialResult, current) -> partialResult + current.toUpperCase());
-
-// P7)
-words.stream().map(String::toUpperCase).reduce(" ", (r, s) -> r + s);
-
-// P8)
-words.stream().reduce((a, b) -> a + "," + b).orElse("");
-
-// P9)
-words.stream().map(words->words.length()).reduce(0, (a, b) -> a + b);
-
-// P10)
-words.stream().filter(s->s.contains("h")).count()
-
-// check out the link at the end of seminar 7
+// P2)
+numbers.stream().filter(s->(s % 4 == 0)).reduce(0, (acc, item) -> acc + (item + 1) % 2)
